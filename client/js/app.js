fcfc346febc38575714211b8d3fcd3a3b2099d2d
meis1124816 <e.e.basler@gmail.com>
2016-03-03 00:39:05 +0000

    router

--- client/js/app.js
+++ client/js/app.js
@@ -11,7 +11,14 @@ function($stateProvider, $urlRouterProvider) {
       templateUrl: '/home.html',
       controller: 'MainCtrl'
     });
-
+    
+  $stateProvider
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+    
   $urlRouterProvider.otherwise('home');
 }]);
 
@@ -32,14 +39,29 @@ app.controller('MainCtrl', [
 'posts',
 function($scope, posts){
   $scope.posts = posts.posts;
+  
+  
   $scope.addPost = function(){
-  if($scope.title != "" && $scope.title) {
-  $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
-  $scope.title = "";
-  $scope.link = "";
+    if($scope.title != "" && $scope.title) {
+      $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
+      $scope.title = "";
+      $scope.link = "";
+    };
   };
-};
+  
   $scope.upvote = function(post){
     post.upvotes+=1;  
   };
+  
+  
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+  
+  
 }]);
